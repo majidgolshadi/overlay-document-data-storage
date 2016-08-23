4002ba64fd90b573f9bbfe79cde22b37c3aabfa4
Majid Golshadi <golshadi.majid@gmail.com>
2016-08-23 16:26:04 +0430

    [Feature] Add update document feature in couchdb

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 'use strict';
 
-var express = require('express'),
-    app = new express();
-
 require('./config.js');
 
-app.get('/', function (req, res) {
-    res.send("some data to show to user");
+const expressjs = require('express'),
+    bodyParser = require('body-parser');
+
+const express = new expressjs(),
+    DB_NAME = 'test_db';
+
+function Application() {
+    var api = undefined,
+        db = undefined;
+}
+
+Application.prototype.init = new Promise(function (resolve, reject) {
+
+    express.use(bodyParser.json());
+    express.use(bodyParser.urlencoded({ extended: true }));
+
+    const couchdb = require('nano')(
+        process.env.OVERLAY_DOCUMENT_DATA_STORAGE_COUCHDB_SERVER
+    );
+
+    couchdb.db.create(DB_NAME, function (err) {
+        express.listen(process.env.OVERLAY_DOCUMENT_DATA_STORAGE_PORT);
+
+        Application.api = express;
+        Application.db = couchdb.use(DB_NAME);
+
+        resolve(Application);
+    });
 });
 
-app.listen(proccess.env.overlay_document_data_storage_port);
+module.exports = new Application();
--- config.js
+++ config.js
@@ -3,6 +3,5 @@
 var fs = require('fs'),
     config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
-process.env['overlay_document_data_storage_port'] = config['port'];
-process.env['overlay_document_data_storage_couchdb_server'] = config['couchdb'];
-process.env['overlay_document_data_storage_overlay_rules'] = config['overlay_rules'];
+process.env['OVERLAY_DOCUMENT_DATA_STORAGE_PORT'] = config['port'];
+process.env['OVERLAY_DOCUMENT_DATA_STORAGE_COUCHDB_SERVER'] = config['couchdb'];
